1ba8614e51c1c60439cc2c94be1d0b418b9bf313
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-09-28 14:59:05 -0400

    Treat lightdp as a module.

--- lightdp/__init__.py
+++ lightdp/__init__.py
@@ -1,2 +1,8 @@
+"""
+LightDP - A Light-Weight Imperative Language That Provides Differential Privacy Proofs.
+"""
+__version__ = '0.1'
+__license__ = 'MIT'
+
 from .scanner import Scanner
 from .parser import Parser
---
+++ lightdp/__main__.py
@@ -0,0 +1,9 @@
+from lightdp import Scanner, Parser
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
