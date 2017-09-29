a29560bae68c7129ef7ba695eea5b8c36a22afe6
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-09-28 23:31:37 -0400

    Fix path of README.md

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 from codecs import open
-from os import path
 import lightdp
 
-here = path.abspath(path.dirname(__file__))
-
 # Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
