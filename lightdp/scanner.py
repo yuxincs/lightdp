4dfa4bca6c52e479408627979c0dc2607071f005
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-10-01 01:03:34 -0400

    Implement scanner.

--- lightdp/scanner.py
+++ lightdp/scanner.py
@@ -1,18 +1,54 @@
-import ply.lex as lex
-#lexer = lex.lex()
-
-
-class Scanner:
-    """ Package the implementation of Scanner
+def build_lexer():
     """
-    def __init__(self):
-        pass
+    Build a ply.lexer for scanning the token stream.
+    :return: ply.lexer
+    """
+    import ply.lex as lex
 
-    def __iter__(self):
-        return lexer.__iter__()
+    reserved = {
+        'if': 'IF',
+        'then': 'THEN',
+        'else': 'ELSE',
+        'true': 'BOOLEAN',
+        'false': 'BOOLEAN',
+        'num': 'NUM_TYPE',
+        'bool': 'BOOL_TYPE',
+        'list': 'LIST_TYPE',
+        'return': 'RETURN',
+        'skip': 'SKIP',
+        'while': 'WHILE',
+        'function': 'FUNCTION'
+    }
 
-    def scan(self, s):
-        pass
-        #print(s)
-        # lexer.input(s)
+    tokens = ['REAL', 'ASSIGN', 'INDUCE', 'IDENTIFIER', 'newline'] + list(set(reserved.values()))
+    literals = "+-*/<>=(),:;[]"
+    t_ignore_COMMENT = r'\#.*'
+    t_ignore_SPACE = r'\s'
 
+    def t_REAL(t):
+        """([1-9]\d*|0)(\.\d+)?"""
+        t.value = float(t.value)
+        return t
+
+    def t_IDENTIFIER(t):
+        """[_a-zA-Z]([0-9]|[a-zA-Z]|_)*"""
+        t.type = reserved.get(t.value, 'IDENTIFIER')
+        return t
+
+    def t_ASSIGN(t):
+        """:="""
+        return t
+
+    def t_INDUCE(t):
+        """"->"""
+        return t
+
+    def t_newline(t):
+        r"""\n+"""
+        t.lexer.lineno += len(t.value)
+
+    def t_error(t):
+        print("Error at line %d: Illegal character '%s'" % (t.lineno, t.value[0]))
+        raise SyntaxError(t)
+
+    return lex.lex()
