d71eacd39aa1a99aaa99a5ee6e783d57ca4121ef
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-10-05 12:40:17 -0400

    Build lexer in test_parser.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,9 @@
+from lightdp.lexer import build_lexer
 from lightdp.parser import build_parser
+import lightdp.ast as ast
 
 
 def test_parser():
+    lexer = build_lexer()
     parser = build_parser()
+
