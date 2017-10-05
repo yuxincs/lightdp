f6ad5066fcecc0ea5dcdc7bc4e6ab9fe903e94f8
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-10-05 19:00:14 -0400

    Add default argument to numpy laplace distribution.

--- lightdp/distributions.py
+++ lightdp/distributions.py
@@ -4,5 +4,5 @@ This module provides a wrapper for numpy.py which keeps track of the weights for
 import numpy.random
 
 
-def lap(loc, scale, size):
-    return numpy.random.laplace(loc, scale, size)
+def lap(scale):
+    return numpy.random.laplace(loc=0.0, scale=scale, size=None)
