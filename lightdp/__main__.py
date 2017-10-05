428add5f8c4d99bf5877e0e3db7a4bab27d57599
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-10-05 16:35:50 -0400

    Write the python codes to the files.

--- lightdp/__main__.py
+++ lightdp/__main__.py
@@ -9,24 +9,30 @@ def main():
     arg_parser = argparse.ArgumentParser(description=__doc__)
     arg_parser.add_argument('files', metavar='FILE', type=str, nargs='+')
     arg_parser.add_argument('-o', '--out',
-                        action='store', dest='out', type=str,
-                        help='The output file name.', required=False)
+                            action='store', dest='out', type=str,
+                            help='The output file name.', required=False)
 
     results = arg_parser.parse_args()
-    if results.out is None:
-        results.out = results.files[0][0:results.files[0].rfind('.')] + '.py'
+
 
     # build lexer and parser
     lexer = build_lexer()
     parser = build_parser()
 
     import jsonpickle
+    all_parse_trees = []
     for file in results.files:
         with open(file, 'r') as f:
             node = parser.parse(f.read(), lexer=lexer)
-            #print(jsonpickle.encode(node))
-            s = ast.transform(node)
-            print(s)
+            if results.out is None:
+                with open(file[0:results.files[0].rfind('.')] + '.py', 'w') as out:
+                    out.write(ast.transform(node))
+            else:
+                all_parse_trees += node
+
+    if results.out is not None:
+        with open(results.out, 'w') as out:
+            out.write(ast.transform(all_parse_trees))
 
 
 if __name__ == '__main__':
