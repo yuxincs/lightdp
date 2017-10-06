2a48c1d4fbe70394368dd190ed63a79be858e643
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-10-05 23:08:26 -0400

    Add test for parser.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,9 +1,14 @@
 from lightdp.lexer import build_lexer
 from lightdp.parser import build_parser
-import lightdp.ast as ast
+import jsonpickle
+import json
 
 
 def test_parser():
     lexer = build_lexer()
     parser = build_parser()
-
+    with open('tests/sparse_vector.ldp', 'r') as f:
+        with open('tests/sparse_vector_ast.json', 'r') as ast:
+            node = parser.parse(f.read(), lexer=lexer)
+            node_dict = json.loads(jsonpickle.encode(node))
+            assert node_dict == json.loads(ast.read())
