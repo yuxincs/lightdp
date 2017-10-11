012e2dd4edeb45696b399353069c24a88ab32a4e
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-10-11 12:04:49 -0400

    Add argument for config files.

--- lightdp/__main__.py
+++ lightdp/__main__.py
@@ -6,18 +6,27 @@ from lightdp import ast
 
 def main():
     import argparse
+    import json
     arg_parser = argparse.ArgumentParser(description=__doc__)
     arg_parser.add_argument('files', metavar='FILE', type=str, nargs='+')
     arg_parser.add_argument('-o', '--out',
                             action='store', dest='out', type=str,
                             help='The output file name.', required=False)
+    arg_parser.add_argument('-c', '--config',
+                            action='store', dest='config', type=str,
+                            help='The configuration file name.', required=False)
 
     results = arg_parser.parse_args()
-
 
     # build lexer and parser
     lexer = build_lexer()
     parser = build_parser()
+
+    # open the config file if present
+    config = None
+    if results.config is not None:
+        with open(results.config, 'r') as f:
+            config = json.loads(f.read())
 
     import jsonpickle
     all_parse_trees = []
@@ -26,13 +35,13 @@ def main():
             node = parser.parse(f.read(), lexer=lexer)
             if results.out is None:
                 with open(file[0:results.files[0].rfind('.')] + '.py', 'w') as out:
-                    out.write(ast.transform(node))
+                    out.write(ast.transform(node, config))
             else:
                 all_parse_trees += node
 
     if results.out is not None:
         with open(results.out, 'w') as out:
-            out.write(ast.transform(all_parse_trees))
+            out.write(ast.transform(all_parse_trees, config))
 
 
 if __name__ == '__main__':
