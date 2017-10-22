d66094c1b70e0a2eec5775228a24a33ba556e7c8
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-10-22 19:40:06 -0400

    Rewrite test_parser.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,14 +1,11 @@
 from lightdp.lexer import build_lexer
 from lightdp.parser import build_parser
 import jsonpickle
-import json
 
 
 def test_parser():
     lexer = build_lexer()
     parser = build_parser()
-    with open('tests/sparse_vector.ldp', 'r') as f:
-        with open('tests/sparse_vector_ast.json', 'r') as ast:
-            node = parser.parse(f.read(), lexer=lexer)
-            node_dict = json.loads(jsonpickle.encode(node))
-            assert node_dict == json.loads(ast.read())
+    precondition, type_map = parser.parse('precondition(q[i] >= -1 and q[i] <= 1); T, N, len, epsilon : num(0)', lexer=lexer)
+    assert precondition == 'q[i] >= -1 and q[i] <= 1'
+    assert jsonpickle.encode(type_map) == """{"N": {"py/object": "lightdp.ast.Type", "left": "num", "right": "0"}, "T": {"py/id": 0}, "epsilon": {"py/id": 0}, "len": {"py/id": 0}}"""
