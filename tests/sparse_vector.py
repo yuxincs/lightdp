fd29b77aa4f2f0e358a705a79f122632fe175aaf
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-10-23 00:01:41 -0400

    Add precondition segment in docstring.

--- tests/sparse_vector.py
+++ tests/sparse_vector.py
@@ -3,6 +3,7 @@ import lightdp
 
 def SparseVector(T, N, len, epsilon, q):
     """
+    precondition(q[i] >= -1 and q[i] <= 1);
     T, N, len, epsilon : num(0); q : list num(*); out : list bool;
     c1, c2, i : num(0); T_threshold, eta_1 : num(1); eta_2 : num(q[i]+eta_2>=T_threshold ? 2:0)
     """
