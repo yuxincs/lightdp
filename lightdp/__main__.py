84b8f2cf7ff649b46f8217297afc4b598002865f
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-10-23 00:04:20 -0400

    Use verifier to verify the program.

--- lightdp/__main__.py
+++ lightdp/__main__.py
@@ -1,7 +1,7 @@
-from lightdp.lexer import build_lexer
-from lightdp.parser import build_parser
+import ast
+import jsonpickle
 from lightdp import __doc__
-from lightdp import ast
+from lightdp import verifier
 
 
 def main():
@@ -18,30 +18,34 @@ def main():
 
     results = arg_parser.parse_args()
 
-    # build lexer and parser
-    lexer = build_lexer()
-    parser = build_parser()
-
     # open the config file if present
     config = None
     if results.config is not None:
         with open(results.config, 'r') as f:
             config = json.loads(f.read())
 
-    import jsonpickle
     all_parse_trees = []
     for file in results.files:
         with open(file, 'r') as f:
-            node = parser.parse(f.read(), lexer=lexer)
-            if results.out is None:
-                with open(file[0:results.files[0].rfind('.')] + '.py', 'w') as out:
-                    out.write(ast.transform(node, config, is_verification=True))
+            tree = ast.parse(f.read())
+            if verifier.verify(tree):
+                print(file + ' : satisfiable')
             else:
-                all_parse_trees += node
+                print(file + ' : unsatisfiable')
+
+            if results.out is None:
+                pass
+                # TODO: write the transformed program to the output file
+                #with open(file[0:results.files[0].rfind('.')] + '.py', 'w') as out:
+                    #out.write(ast.transform(node, config, is_verification=True))
+            else:
+                all_parse_trees += tree
 
     if results.out is not None:
-        with open(results.out, 'w') as out:
-            out.write(ast.transform(all_parse_trees, config, is_verification=True))
+        pass
+        # TODO: write the transformed program to the output file
+        #with open(results.out, 'w') as out:
+            #out.write(ast.transform(all_parse_trees, config, is_verification=True))
 
 
 if __name__ == '__main__':
