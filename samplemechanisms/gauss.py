8dc0b42bb8ed31fe180d4c0c7a823596c9e1c992
Daniel Kifer <dkifer@dkifer.cse.psu.edu>
2017-10-24 13:41:13 -0400

    added sample usage in comments

--- samplemechanisms/gauss.py
+++ samplemechanisms/gauss.py
@@ -1,6 +1,16 @@
 import numpy
 
 #neighboring inputs change by 1
-def mech(eps, x):
+# normally prng = numpy.random.RandomState(seed) or numpy.random
+#
+# in normal operation, this would be called as
+# prng = numpy.random.RandomState(seed)
+# result = main(eps, x, prng)
+#
+#in statistical debugging mode, this would be called
+# prng = lightdbstat.ImportanceSampler(params go here)
+# result = main(eps, x, prng)
+# prng.getweight()
+def mech(eps, x, prng):
     sigma = numpy.sqrt(2.0)/eps
-    return numpy.random.normal(scale=sigma) + x
+    return prng.normal(scale=sigma) + x
