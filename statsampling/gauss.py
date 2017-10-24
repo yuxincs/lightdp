3136d6fc9c3e4f401f07c170631a6714aef3690a
Ding Ding <dingsquared@gmail.com>
2017-10-24 12:26:19 -0400

    modified sampling

--- statsampling/gauss.py
+++
@@ -1,6 +0,0 @@
-import numpy
-
-#neighboring inputs change by 1
-def mech(eps, x):
-    sigma = numpy.sqrt(2.0)/eps
-    return numpy.random.normal(scale=sigma) + x
--- statsampling/nm3.py
+++
@@ -1,7 +0,0 @@
-import numpy
-
-#neighboring inputs change by at most 1 in exactly one list element. x is a list
-def mech(eps, x):
-    y = [a + numpy.random.laplace(scale=1.0/eps) for a in x]
-    ind = numpy.argmax(y)
-    return y[ind]
