fad0d1148965f7c92ab0d25bf1d8bc22d16808c6
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-11-06 20:47:13 -0500

    Add FunctionType.

--- lightdp/typing.py
+++ lightdp/typing.py
@@ -5,9 +5,15 @@ class NumType:
 
 class ListType:
     def __init__(self, elem_type):
-        self.elem = elem_type
+        self.elem_type = elem_type
 
 
 class BoolType:
     def __init__(self):
         pass
+
+
+class FunctionType:
+    def __init__(self, para_type, return_type):
+        self.para_type = para_type
+        self.return_type = return_type
