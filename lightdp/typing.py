294e900a622606f7b49d828a8bdf268e3f3eb696
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-11-14 13:04:08 -0500

    Use REAL to indicate number in all cases.

--- lightdp/typing.py
+++ lightdp/typing.py
@@ -24,9 +24,9 @@ class FunctionType:
 
 def to_smt_type(lightdp_type):
     if isinstance(lightdp_type, NumType):
-        return pysmt.typing.INT
+        return pysmt.typing.REAL
     elif isinstance(lightdp_type, ListType):
-        return pysmt.typing.ArrayType(pysmt.typing.INT, to_smt_type(lightdp_type.elem_type))
+        return pysmt.typing.ArrayType(pysmt.typing.REAL, to_smt_type(lightdp_type.elem_type))
     elif isinstance(lightdp_type, FunctionType):
         return pysmt.typing.FunctionType(to_smt_type(lightdp_type.return_type), to_smt_type(lightdp_type.para_type))
     elif isinstance(lightdp_type, BoolType):
