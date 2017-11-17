49d55b06f1d47254ea40bfeb72536537b886f761
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-11-17 16:35:17 -0500

    Change to simple call Laplace.

--- tests/sparse_vector.py
+++ tests/sparse_vector.py
@@ -5,13 +5,13 @@ def SparseVector(T, N, len, epsilon, q):
     c1, c2, i : num(0); T_threshold, eta_1 : num(1); eta_2 : num(2 if q[i]+eta_2>=T_threshold else 0)
     """
     out = []
-    eta_1 = lightdp.distributions.sample('Lap')(2 / epsilon)
+    eta_1 = Laplace(2 / epsilon)
     T_threshold = T + eta_1
     c1 = 0
     c2 = 0
     i = 0
     while c1 < N and i < len:
-        eta_2 = lightdp.distributions.sample('Lap')(4 * N/epsilon)
+        eta_2 = Laplace(4 * N/epsilon)
         if q[i] + eta_2 >= T_threshold:
             out = (out + [True])
             c1 = c1 + 1
