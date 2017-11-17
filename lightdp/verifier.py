a5083497b317e581fb5bc584a8e27fba0ecd8d81
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-11-17 16:35:43 -0500

    Implement the verifier.

--- lightdp/verifier.py
+++ lightdp/verifier.py
@@ -1,5 +1,6 @@
 import ast
 import _ast
+import re
 import pysmt.shortcuts as shortcuts
 from lightdp.typing import *
 
@@ -34,7 +35,7 @@ unary_operation_map = {
 
 # translate the expression ast into pysmt constraints
 class ExpressionTranslator(ast.NodeVisitor):
-    def __init__(self, type_map, distance_vars = set()):
+    def __init__(self, type_map, distance_vars=set()):
         self.type_map = type_map
         self.__distance_vars = distance_vars
 
@@ -90,8 +91,10 @@ class ExpressionTranslator(ast.NodeVisitor):
 
 
 class NodeVerifier(ast.NodeVisitor):
-    def __init__(self):
-        self.__environments = []
+    def __init__(self, constraints):
+        assert isinstance(constraints, list)
+        self.__constraints = constraints
+        self.__type_map = {}
 
     @staticmethod
     def parse_docstring(s):
@@ -105,20 +108,111 @@ class NodeVerifier(ast.NodeVisitor):
     def visit_FunctionDef(self, node):
         annotation = NodeVerifier.parse_docstring(ast.get_docstring(node))
         if annotation is not None:
-            self.__environments.append(annotation)
-            super(NodeVerifier, self).generic_visit(node)
-            self.__environments.pop()
+            precondition, self.__type_map = annotation
+            res = re.findall(r"""\^([_a-zA-Z][0-9a-zA-Z_]*)""", precondition)
+            self.__constraints.append(ExpressionTranslator(self.__type_map, set(res)).visit(ast.parse(precondition.replace('^', ''))))
+
+            for name, var_type in dict(self.__type_map).items():
+                if not name[0] == '^' and isinstance(var_type, NumType):
+                    if isinstance(self.__type_map[name], (NumType, BoolType, FunctionType)):
+                        self.__type_map['^' + name] = NumType(0)
+                    elif isinstance(self.__type_map[name[1:]], ListType):
+                        self.__type_map['^' + name] = ListType(NumType(0))
+
+                    constraint = shortcuts.Equals(shortcuts.Symbol('^' + name, to_smt_type(self.__type_map['^' + name])),
+                                                  ExpressionTranslator(self.__type_map).visit(ast.parse(var_type.value)))
+
+                    self.__constraints.append(constraint)
+            self.generic_visit(node)
+
+    def visit_If(self, node):
+        self.__constraints.append(shortcuts.Equals(self.visit(node.test)[0], self.visit(node.test)[1]))
+        self.generic_visit(node)
+
+    def visit_Compare(self, node):
+        assert len(node.ops) == 1 and len(node.comparators), "Only allow one comparators in binary operations."
+        left_expr = self.visit(node.left)
+        right_expr = self.visit(node.comparators[0])
+        return (dot_operation_map[node.ops[0].__class__](left_expr[0], right_expr[0]),
+                dot_operation_map[node.ops[0].__class__](shortcuts.Plus(left_expr[0], left_expr[1]),
+                                                         shortcuts.Plus(right_expr[0], right_expr[1])))
+
+    def visit_Name(self, node):
+        assert node.id in self.__type_map, 'Undefined %s' % node.id
+        #if isinstance(self.__type_map[node.id], NumType):
+        return (shortcuts.Symbol(node.id, to_smt_type(self.__type_map[node.id])),
+                shortcuts.Symbol('^' + node.id, to_smt_type(self.__type_map['^' + node.id])))
+                    #shortcuts.Plus(shortcuts.Symbol(node.id, to_smt_type(self.__type_map[node.id])),))
+        #else:
+            #return (shortcuts.Symbol(node.id, to_smt_type(self.__type_map[node.id])),
+                  #  shortcuts.Symbol(node.id, to_smt_type(self.__type_map[node.id])))
+
+    def visit_Num(self, node):
+        return shortcuts.Real(node.n), shortcuts.Real(0)
+
+    def visit_BinOp(self, node):
+        if isinstance(node.op, tuple(oplus_operation_map.keys())):
+            return (oplus_operation_map[node.op.__class__](self.visit(node.left)[0], self.visit(node.right)[0]),
+                    oplus_operation_map[node.op.__class__](self.visit(node.left)[1], self.visit(node.right)[1]))
+        elif isinstance(node.op, tuple(otimes_operation_map.keys())):
+            return (otimes_operation_map[node.op.__class__](self.visit(node.left)[0], self.visit(node.right)[0]),
+                    otimes_operation_map[node.op.__class__](self.visit(node.left)[1], self.visit(node.right)[1]))
+
+    def visit_Subscript(self, node):
+        assert isinstance(node.slice, ast.Index), "Only index is supported."
+        return (shortcuts.Select(self.visit(node.value)[0], self.visit(node.slice.value)[0]),
+                shortcuts.Select(self.visit(node.value)[1], self.visit(node.slice.value)[0]))
+
+    def visit_BoolOp(self, node):
+        assert isinstance(node.op, tuple(bool_operation_map.keys()))
+        return (bool_operation_map[node.op.__class__]([self.visit(value)[0] for value in node.values]),
+                bool_operation_map[node.op.__class__]([self.visit(value)[1] for value in node.values]))
+
+    def visit_UnaryOp(self, node):
+        assert isinstance(node.op, tuple(unary_operation_map.keys()))
+        return (unary_operation_map[node.op.__class__](self.visit(node.operand)[0]),
+                unary_operation_map[node.op.__class__](self.visit(node.operand)[1]))
 
     def visit_Assign(self, node):
-        for target in node.targets:
-            if target.id in self.__environments[len(self.__environments) - 1][1]:
-                print(type(node.value))
+        if isinstance(node.value, ast.Call) and node.value.func.id == 'Laplace':
+            pass
+        elif len(node.targets) == 1 and isinstance(node.targets[0], ast.Name):
+            target_type = self.__type_map[node.targets[0].id]
+            if isinstance(target_type, ListType):
+                # TODO: list assignment
+                pass
+            else:
+                self.__constraints.append(shortcuts.Equals(self.visit(node.targets[0])[1], self.visit(node.value)[1]))
+        else:
+            assert False, 'Currently don\'t support multiple assignment.'
+
+    def visit_NameConstant(self, node):
+        assert node.value == True or node.value == False, 'Unsupported NameConstant %s' % str(node.value)
+        return node.value, NumType(0)
+
+    def visit_Call(self, node):
+        if isinstance(node.func, ast.Attribute) and node.func.attr == 'append':
+            # type check
+            assert isinstance(self.__type_map[node.func.value.id], ListType), \
+                '%s is not typed as list.' % node.func.value.id
+            if isinstance(self.__type_map[node.func.value.id].elem_type, NumType):
+                self.__constraints.append(shortcuts.Equals(shortcuts.Select(self.visit(node.func.value.id)[1], self.visit(ast.Name('i'))), self.visit(node.args[0])[1]))
+
+        else:
+            # TODO: check the function return type.
+            pass
 
 
 def verify(tree):
     assert isinstance(tree, _ast.AST)
     for node in ast.walk(tree):
         if isinstance(node, ast.FunctionDef):
-            verifier = NodeVerifier()
-            verifier.visit(node)
-            # TODO: do type inference here
+            # TODO: consider multiple functions scenario
+            constraints = []
+            NodeVerifier(constraints).visit(node)
+            final_constraints = shortcuts.And(constraints[0], shortcuts.Not(shortcuts.And(constraints[1:])))
+            from pysmt.printers import HRPrinter
+            import sys
+            printer = HRPrinter(sys.stdout)
+            printer.printer(final_constraints)
+            return shortcuts.is_sat(final_constraints)
