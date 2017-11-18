b95e82cb34a7227f7212ee44bb77c6652924c783
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-11-18 15:09:44 -0500

    Implement transfomer.

---
+++ lightdp/transformer.py
@@ -0,0 +1,62 @@
+import ast
+
+
+_noise_functions = ('Laplace', )
+
+
+class NodeTransformer(ast.NodeTransformer):
+    def visit_Module(self, node):
+        new_node = self.generic_visit(node)
+        new_node.body.insert(0, ast.FunctionDef(name='havoc',
+                                                args=ast.arguments(
+                                                    args=[ast.Name(id='scale', ctx=ast.Param())],
+                                                    vararg=None,
+                                                    kwarg=None,
+                                                    defaults=[]),
+                                                decorator_list=[],
+                                                body=[ast.Expr(value=ast.Str('Implement the havoc instruction here')),
+                                                      ast.Pass()]
+                                                ))
+        import astunparse
+        print(astunparse.dump(new_node))
+        return new_node
+
+    def visit_FunctionDef(self, node):
+        new_node = self.generic_visit(node)
+        # TODO: determine whether it's the function to transform or not
+        # remove the annotation string
+        new_node.body = new_node.body[1:]
+        new_node.body.insert(0, ast.Assign(targets=[ast.Name(id='__V_epsilon', ctx=ast.Store())], value=ast.Num(0)))
+
+        return new_node
+
+    def visit_Assign(self, node):
+        if isinstance(node.value, ast.Call) \
+                and isinstance(node.value.func, ast.Name) \
+                and node.value.func.id in _noise_functions:
+            node.value.func.id = 'havoc'
+            change_v_epsilon = ast.AugAssign(target=ast.Name(id='__V_epsilon', ctx=ast.Store()), op=ast.Add(),
+                                             value=ast.BinOp(left=ast.Num(1.0), op=ast.Div(), right=node.value.args[0]))
+            return change_v_epsilon, node
+        else:
+            return node
+
+    def generic_visit(self, node):
+        new_node = super(NodeTransformer, self).generic_visit(node)
+        if hasattr(new_node, 'body') and isinstance(new_node.body, (tuple, list)):
+            # unpack the tuple that visit_Assign created
+            unpacked_body = []
+            for node in new_node.body:
+                if isinstance(node, (tuple, list)):
+                    for n in node:
+                        unpacked_body.append(n)
+                else:
+                    unpacked_body.append(node)
+
+            new_node.body = unpacked_body
+
+        return new_node
+
+
+def transform(node):
+    return NodeTransformer().visit(node)
