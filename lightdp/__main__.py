b5774b463ee9ad093e5ec11443582e5adcdae9db
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-11-18 15:24:05 -0500

    Write the transformed program to file.

--- lightdp/__main__.py
+++ lightdp/__main__.py
@@ -1,12 +1,13 @@
 import ast
-import jsonpickle
 from lightdp import __doc__
-from lightdp import verifier
+from lightdp import verifier, transformer
 
 
 def main():
     import argparse
     import json
+    import astunparse
+
     arg_parser = argparse.ArgumentParser(description=__doc__)
     arg_parser.add_argument('files', metavar='FILE', type=str, nargs='+')
     arg_parser.add_argument('-o', '--out',
@@ -33,19 +34,19 @@ def main():
             else:
                 print(file + ' : unsatisfiable')
 
+            transformed = transformer.transform(tree)
             if results.out is None:
-                pass
-                # TODO: write the transformed program to the output file
-                #with open(file[0:results.files[0].rfind('.')] + '.py', 'w') as out:
-                    #out.write(ast.transform(node, config, is_verification=True))
+                # write the transformed program to the output file
+                with open(file[0:results.files[0].rfind('.')] + '.t.py', 'w') as out:
+                    out.write(astunparse.unparse(transformed))
             else:
-                all_parse_trees += tree
+                all_parse_trees.append(transformed)
 
     if results.out is not None:
-        pass
         # TODO: write the transformed program to the output file
-        #with open(results.out, 'w') as out:
-            #out.write(ast.transform(all_parse_trees, config, is_verification=True))
+        with open(results.out, 'w') as out:
+            for transformed in transformer.transform(all_parse_trees):
+                out.write(astunparse.unparse(transformed))
 
 
 if __name__ == '__main__':
