410dcb901b7a2b23f22b17cc321134b6f4c74a4d
Yuxin Wang <yuxinwang.dev@gmail.com>
2017-12-29 13:46:35 -0500

    Only allow one input file and add json-file output.

--- lightdp/__main__.py
+++ lightdp/__main__.py
@@ -12,38 +12,39 @@ def main():
     import astunparse
 
     arg_parser = argparse.ArgumentParser(description=__doc__)
-    arg_parser.add_argument('files', metavar='FILE', type=str, nargs='+')
+    arg_parser.add_argument('file', metavar='FILE', type=str, nargs='1')
     arg_parser.add_argument('-o', '--out',
                             action='store', dest='out', type=str,
                             help='The output file name.', required=False)
-    arg_parser.add_argument('-c', '--config',
-                            action='store', dest='config', type=str,
-                            help='The configuration file name.', required=False)
+    arg_parser.add_argument('-j', '--json',
+                            action='store', dest='json_file', type=str,
+                            help='Output all results in json format.', required=False)
 
     results = arg_parser.parse_args()
 
-    all_parse_trees = []
-    for file in results.files:
-        with open(file, 'r') as f:
-            tree = ast.parse(f.read())
-            if verifier.verify(tree):
-                print(file + ' : satisfiable')
-            else:
-                print(file + ' : unsatisfiable')
+    results.out = results.file[0:results.file.rfind('.')] + '_t.py' if results.out is None else results.out
 
-            transformed = transformer.transform(tree)
-            if results.out is None:
-                # write the transformed program to the output file
-                with open(file[0:results.files[0].rfind('.')] + '_t.py', 'w') as out:
-                    out.write(astunparse.unparse(transformed))
-            else:
-                all_parse_trees.append(transformed)
+    # parse / verify / transform the file
+    with open(results.file, 'r') as f:
+        tree = ast.parse(f.read())
+        is_sat, constraints = verifier.verify(tree)
+        transformed = None
+        if is_sat:
+            with open(results.out, 'w') as out:
+                transformed = transformer.transform(tree)
+                out.write(transformed)
 
-    if results.out is not None:
-        # TODO: write the transformed program to the output file
-        with open(results.out, 'w') as out:
-            for transformed in transformer.transform(all_parse_trees):
-                out.write(astunparse.unparse(transformed))
+        if results.json_file is not None:
+            with open(results.json_file, 'w') as out:
+                import json
+                json.dump(out, {
+                    'is_sat': is_sat,
+                    'constraints': constraints,
+                    'transformed': transformed
+                })
+
+        # print('\033[32;1mFinal Constraint:\033[0m')
+        # print(in_file + ' : satisfiable' if is_sat else ' : unsatisfiable')
 
 
 if __name__ == '__main__':
