02044b3ba8936ab630e380b6459e79972c330cf9
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-01-13 00:33:05 -0500

    Create an abstract layer for hypothesis testing for different algorithms.

--- scripts/hypothesis_test.py
+++ scripts/hypothesis_test.py
@@ -2,17 +2,6 @@ import numpy as np
 from collections import Counter
 import multiprocessing as mp
 import math
-
-
-# Create two values of input which satisfies the property of input query, along with some hyperparameters
-D1 = [5.75, 4.34, 6.15, 3.2, 5.8, 4.6, 3.56, 6.24, 5.86, 5.68]
-D2 = [5.21, 3.52, 5.57, 3.18, 5.6, 5.1, 4.15, 5.72, 5.99, 5.32]
-diff = [a - b for a, b in zip(D1, D2)]
-eps1 = 4
-eps2 = 2
-T = 5
-N = 2
-n = 10000
 import os
 
 
@@ -51,7 +40,8 @@ def sparsevector(T, N, eps, q):
     return len(out)
 
 
-def test_stat(x, y, eps):
+# TODO: eps can be designed cleaner
+def test_stat(x, y, n, eps=2):
     """
         Function to compute test statistic T
     :param x: input array
@@ -61,137 +51,127 @@ def test_stat(x, y, eps):
     return (len(x) / n) - (np.exp(eps)) * (len(y) / n)  # one of the options
 
 
-def sig_test_stat(R, eps):
-    """
-        Function to compute significance of the value of T
-    :param R: x and y
-    :return: test statistic
-    """
-    X = []
-    Y = []
-    for i in R:
-        if np.exp(eps) / (1 + (np.exp(eps))) >= np.random.random():
-            X.append(i)
-        else:
-            Y.append(i)
-    return test_stat(X, Y, eps)
-
-
-    # to find a reasonable S
-
-    # for eps in range(2,10):
-    #     A=[]
-    #     B=[]
-    #     for i in range(100000):
-    #         A.append(noisymax(D1,eps))
-    #         B.append(noisymax(D2,eps))
-    #     print(Counter(A+B))
-
-    def calc_xy(id, func, D1, D2, S, eps1, eps2, n, result_queue):
-        print('[Process-%d] Started with %d' % (id, n))
+def hypothesis_test(algorithm, D1, D2, args, iterations, stat_test_func, S_selector, cores=0):
+    def calc(p_id, func, D1, D2, S, args, n, result_queue):
+        # print('[Process-%d] Started with %d' % (id, n))
+        # reseed the numpy random module
         np.random.seed(int(os.urandom(4).encode('hex'), 16))
         x = []
         y = []
         for _ in range(n):
-            a = func(D1, eps1)
-            b = func(D2, eps1)
+            a = func(D1, *args)
+            b = func(D2, *args)
             if a in S:
                 x.append(a)
             if b in S:
                 y.append(b)
 
-        T1 = test_stat(x, y, eps2)
-        T2 = test_stat(y, x, eps2)
+        T1 = stat_test_func(x, y, iterations)
+        T2 = stat_test_func(y, x, iterations)
 
         # compute significance of test statistic
+        # TODO : eps argument can be designed cleaner
+        def sig_test_stat(R, eps=2):
+            """
+                Function to compute significance of the value of T
+            :param R: x and y
+            :return: test statistic
+            """
+            X = []
+            Y = []
+            for i in R:
+                if np.exp(eps) / (1 + (np.exp(eps))) >= np.random.random():
+                    X.append(i)
+                else:
+                    Y.append(i)
+            return stat_test_func(X, Y, iterations, eps)
+
         R = x + y
         ti1 = []
         ti2 = []
-        for i in range(n):
-            ti1.append(sig_test_stat(R, eps2))
-            ti2.append(sig_test_stat(R, eps2))
+        for _ in range(n):
+            ti1.append(sig_test_stat(R))
+            ti2.append(sig_test_stat(R))
 
         result_queue.put((sum([x >= T1 for x in ti1]), sum([x >= T2 for x in ti2])))
         # print('[Process-%d] Finished' % id)
         return
 
-    S = [7, 4, 2]
+    # compute test statistics
+    process_count = mp.cpu_count() if cores == 0 else cores
+    result_queue = mp.Queue()
+    processes = []
+    for p_id in range(process_count):
+        process_iterations = int(math.floor(float(iterations) / process_count))
+        process_iterations += iterations % process_iterations if p_id == process_count - 1 else 0
+        process = mp.Process(target=calc, args=(p_id, algorithm, D1, D2, S_selector(algorithm), args,
+                                                process_iterations, result_queue))
+        processes.append(process)
+        process.start()
 
-    # noisymax:
-    for eps1 in range(2, 10):
-        # compute test statistic
-        result_queue = mp.Queue()
-        processes = []
+    # print('Generate %d processes for %s function' % (mp.cpu_count(), 'NoisyMax'))
 
-        for i in range(mp.cpu_count()):
-            process = mp.Process(target=calc_xy, args=(i, noisymax, D1, D2, S, eps1, eps2,
-                                                       int(math.ceil(float(n) / mp.cpu_count())), result_queue))
-            processes.append(process)
-            process.start()
+    for process in processes:
+        process.join()
 
-        # print('Generate %d processes for %s function' % (mp.cpu_count(), 'NoisyMax'))
+    # print('Work finished, merge the results')
 
-        for process in processes:
-            process.join()
+    # merge the results
+    value1 = 0
+    value2 = 0
+    for _ in range(process_count):
+        local_v1, local_v2 = result_queue.get()
+        value1 += local_v1
+        value2 += local_v2
 
-        # print('Work finished, merge the results')
-
-        # merge the results
-        value1 = 0
-        value2 = 0
-        for _ in range(len(processes)):
-            local_v1, local_v2 = result_queue.get()
-            value1 += local_v1
-            value2 += local_v2
-
-        result = [eps1, float(value1) / n, float(value2) / n]
-        # compute p value
-        print(str(result))
-
-    # find appropriate S for sparse vector
-    # for eps in range(2,10):
-    #     A=[]
-    #     B=[]
-    #     for i in range(100000):
-    #         A.append(sparsevector(T,N,4,D1))
-    #         B.append(sparsevector(T,N,4,D2))
-    #     print(Counter(A+B))
-
-    S = [3, 2]
-    # TODO: sparse vector can be merged
-    # sparse vector:
-    """
-    for eps1 in range(2,10):
-        # compute test statistic
-        x=[]
-        y=[]
-        for i in range(n):
-            a=sparsevector(T,N,eps1,D1)
-            b=sparsevector(T,N,eps1,D2)
-            if a in S:
-                x.append(a)
-            if b in S:
-                y.append(b)
-        T1=test_stat(x,y,eps2)
-        T2=test_stat(y,x,eps2)
-
-            # compute significance of test statistic
-
-        R=x+y
-        ti1=[]
-        ti2=[]
-        for i in range(n):
-            ti1.append(sig_test_stat(R,eps2))
-            ti2.append(sig_test_stat(R,eps2))
-
-        result=[eps1,sum([x>=T1 for x in ti1])/n,sum([x>=T2 for x in ti2])/n]
-        # compute p value
-        print(str(result))
-        """
+    # compute p value
+    return args[0], float(value1) / iterations, float(value2) / iterations
 
 
 def main():
+    # create two values of input which satisfies the property of input query, along with some hyperparameters
+    D1 = [5.75, 4.34, 6.15, 3.2, 5.8, 4.6, 3.56, 6.24, 5.86, 5.68]
+    D2 = [5.21, 3.52, 5.57, 3.18, 5.6, 5.1, 4.15, 5.72, 5.99, 5.32]
     # check if input queries are valid to use
     assert all(abs(a - b) <= 1 for a, b in zip(D1, D2))
+    T = 5
+    N = 2
+    n = 1000
+
+    # hand-picked S for the two algorithms
+    def S_selector(algorithm):
+        # to find a reasonable S
+
+        # for eps in range(2,10):
+        #     A=[]
+        #     B=[]
+        #     for i in range(100000):
+        #         A.append(noisymax(D1,eps))
+        #         B.append(noisymax(D2,eps))
+        #     print(Counter(A+B))
+
+        # find appropriate S for sparse vector
+        # for eps in range(2,10):
+        #     A=[]
+        #     B=[]
+        #     for i in range(100000):
+        #         A.append(sparsevector(T,N,4,D1))
+        #         B.append(sparsevector(T,N,4,D2))
+        #     print(Counter(A+B))
+        from inspect import isfunction
+
+        assert isfunction(algorithm)
+        if algorithm.__name__ == 'noisymax':
+            return [7, 4, 2]
+        if algorithm.__name__ == 'sparsevector':
+            return [3, 2]
+
+    # NoisyMax test
+    for eps in range(7, 15):
+        res = hypothesis_test(noisymax, D1, D2, (eps, ), n, test_stat, S_selector, cores=1)
+        print(res)
+    # TODO: bring back the sprase vector test
+
+
 if __name__=="__main__":
-   main()
+    main()
