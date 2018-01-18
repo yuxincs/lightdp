70002c27a3fdca3a1f667089aea2ff28cae9f318
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-01-17 19:37:01 -0500

    Remove unused files.

--- lightdp/stattest/nm2.py
+++
@@ -1,7 +0,0 @@
-import numpy
-
-#neighboring inputs change by at most 1 in exactly one list element. x is a list
-def mech(eps, x, prng):
-    y = [a + prng.laplace(scale=1.0/eps) for a in x]
-    ind = numpy.argmax(y)
-    return x[ind] + prng.laplace(scale=1.0/eps)
