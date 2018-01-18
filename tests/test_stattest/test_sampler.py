7c5588943e3fa30ffaf3a0646f7171616da4a0a7
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-01-17 21:26:34 -0500

    Add a test case.

--- tests/test_stattest/test_sampler.py
+++ tests/test_stattest/test_sampler.py
@@ -9,3 +9,5 @@ def test_prng():
     prng.add_config('laplace', 'normal')
     assert isinstance(prng.laplace(0, 1), float)
     assert prng.weight() != 1
+    prng.add_config('normal', 'laplace', lambda x: x - 1, lambda x: x + 1)
+    assert isinstance(prng.normal(0, 1), float)
