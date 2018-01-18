af893e1aa85e7243360ca536439b2a9b0974becc
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-01-17 19:44:56 -0500

    Rename the method get_weight to weight.

--- lightdp/stattest/sampler.py
+++ lightdp/stattest/sampler.py
@@ -28,12 +28,12 @@ class Sampler:
         except ValueError as err:
             print(err.args)
             exit(1)
-        self.weight = 1
+        self._weight = 1
         self.mu = lambda x: x
         self.sigma = lambda x: x
 
-    def get_weight(self):
-        return self.weight
+    def weight(self):
+        return self._weight
 
     def add_config(self, new, f=lambda x: x, g=lambda x: x):
         self.rng = get_rng(new)
@@ -45,12 +45,12 @@ class Sampler:
         m = self.mu(loc)
         s = self.sigma(scale)
         r = self.rng(m, s)
-        self.weight *= stats.laplace.pdf(r, m, s) / self.pdf(r, m, s)
+        self._weight *= stats.laplace.pdf(r, m, s) / self.pdf(r, m, s)
         return r
 
     def normal(self, loc=0.0, scale=1.0):
         m = self.mu(loc)
         s = self.sigma(scale)
         r = self.rng(m, s)
-        self.weight *= stats.norm.pdf(r, m, s) / self.pdf(r, m, s)
+        self._weight *= stats.norm.pdf(r, m, s) / self.pdf(r, m, s)
         return r
--- tests/test_stattest.py
+++ tests/test_stattest.py
@@ -7,8 +7,8 @@ def test_prng():
     prng2 = sampler.Sampler('laplace')
     assert isinstance(prng.laplace(0, 1), float)
     assert isinstance(prng2.laplace(0, 1), float)
-    assert prng.weight != 1
-    assert prng2.weight == 1
+    assert prng.weight() != 1
+    assert prng2.weight() == 1
     assert isinstance(gauss.mech(1, 0, prng), float)
     assert isinstance(lapmech.mech(1, 0, prng), float)
     assert isinstance(nm.mech(1, [1.0, 2.0, 3.0], prng), float)
