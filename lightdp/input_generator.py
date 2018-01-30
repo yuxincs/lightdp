f3f0a88040de5d8f76db7f5a57dee42ee2f8fdb0
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-01-30 13:49:06 -0500

    Rename the package name and add method notes.

--- lightdp/input_generator.py
+++ lightdp/input_generator.py
@@ -4,12 +4,13 @@ from lightdp.typing import *
 from .verifier import NodeVerifier
 
 
-class CounterExampleGenerator(NodeVerifier):
+class InputGenerator(NodeVerifier):
     def __init__(self, args):
-        super(CounterExampleGenerator, self).__init__()
+        super(InputGenerator, self).__init__()
         assert isinstance(args, dict)
         self._args = args
         self._db_var = None
+        self._if_not_checks = []
 
     def visit_FunctionDef(self, node):
         self._db_var = node.args.args[-1].arg
@@ -27,7 +28,7 @@ class CounterExampleGenerator(NodeVerifier):
                 if isinstance(var_type, NumType):
                     self._type_map['^' + name] = NumType(0)
                     if var_type.value != '*':
-                        constraint = self._symbol('^' + name) == self.visit(self.parse_expr(var_type.value))[0]
+                        constraint = self._symbol('^' + name) == 0
                 elif isinstance(var_type, BoolType):
                     self._type_map['^' + name] = NumType(0)
                     constraint = self._symbol('^' + name) == 0
@@ -58,19 +59,34 @@ class CounterExampleGenerator(NodeVerifier):
 
             # empty the check list
             del self._if_checks[:]
+            del self._if_not_checks[:]
 
             self.generic_visit(node)
 
     def visit_Assign(self, node):
         if isinstance(node.value, ast.Call) and node.value.func.id == 'Laplace':
             self._declarations.append(self._symbol(node.targets[0].id) == 0)
-        super(CounterExampleGenerator, self).visit_Assign(node)
+        elif len(node.targets) == 1 and isinstance(node.targets[0], ast.Name):
+            target_type = self._type_map[node.targets[0].id]
+            if isinstance(target_type, ListType):
+                # TODO: list assignment
+                pass
+                # raise NotImplementedError('List assignment not implemented.')
+            else:
+                for child in ast.iter_child_nodes(node.value):
+                    if isinstance(child, ast.Name) and child.id in self._args.keys():
+                        self._declarations.append(self.visit(node.targets[0])[0] == self.visit(node.value)[0])
+                        break
+        else:
+            raise NotImplementedError('Currently don\'t support multiple assignment.')
+
+        super(InputGenerator, self).visit_Assign(node)
 
     def get_constraint(self):
         for k, v in self._args.items():
             self._declarations.append(self._symbol(k) == v)
         final_constraint = z3.And(z3.And(self._precondition), z3.And(self._declarations), z3.And(self._assign_checks))
-        return final_constraint, self._if_checks, self._symbol(self._db_var), self._symbol('^' + self._db_var)
+        return final_constraint, self._if_checks, self._if_not_checks, self._symbol(self._db_var), self._symbol('^' + self._db_var)
 
     def visit_Subscript(self, node):
         # substitute the list-typed db_var with a single variable
@@ -78,31 +94,58 @@ class CounterExampleGenerator(NodeVerifier):
             return (self.visit(node.value)[0],
                     self.visit(node.value)[1])
         else:
-            return super(CounterExampleGenerator, self).visit_Subscript(node)
+            return super(InputGenerator, self).visit_Subscript(node)
+
+    def visit_If(self, node):
+        test_node = self.visit(node.test)
+        self._if_checks.append(test_node[0] == test_node[1])
+        self._if_not_checks.append(test_node[0] != test_node[1])
+        self.generic_visit(node)
+
+    def visit_IfExp(self, node):
+        test_node = self.visit(node.test)
+        self._if_checks.append(test_node[0] == test_node[1])
+        self._if_not_checks.append(test_node[0] != test_node[1])
+        return z3.If(test_node[0], self.visit(node.body)[0], self.visit(node.orelse)[0]), \
+               z3.If(test_node[1], self.visit(node.body)[1], self.visit(node.orelse)[1])
 
 
-def generate_inputs(tree, check_array):
+def generate_inputs(tree, args, num_input, fixed_d1=[]):
+    """
+    :param tree: The algorithm's AST tree
+    :param args: The argument to be put in the algorithm
+    :param num_input: The number of inputs to generate
+    :param fixed_d1: Optional, will only generate database 2 according to the given database 1 if provided
+    :return:
+    """
     import _ast
     assert isinstance(tree, _ast.AST)
-    generator = CounterExampleGenerator({'T': 10})
-    generator.visit(tree)
-    constraint, checks, db_var, db_distance_var = generator.get_constraint()
+    assert len(fixed_d1) == 0 or num_input == len(fixed_d1), "num_input and len(fixed_d1) must match"
 
-    d1 = []
+    generator = InputGenerator(args)
+    generator.visit(tree)
+    constraint, checks, not_checks, db_var, db_distance_var = generator.get_constraint()
+
+    d1 = fixed_d1
     d2 = []
     s = z3.Solver()
+    # TODO: think of a good way for control parameters
+    check_array = [False for _ in range(num_input)]
 
-    for i in range(len(check_array)):
+    for i in range(len(d1)):
+        s.add(z3.And(constraint))
         s.push()
-        s.add(z3.And(constraint, z3.And(checks) if check_array[i] else z3.Not(z3.And(checks))))
+        s.add(db_var == d1[i])
+        s.add(db_distance_var != 0)
+        s.add(z3.And(checks) if check_array[i] else z3.And(not_checks))
         if s.check() == z3.sat:
-            d1.append(float(s.model()[db_var].as_decimal(5)))
-            d2.append(float(s.model()[db_var].as_decimal(5)) + float(s.model()[db_distance_var].as_decimal(5)))
+            d2.append(float(s.model()[db_var].as_decimal(5).replace('?', '')) +
+                      float(s.model()[db_distance_var].as_decimal(5).replace('?', '')))
             s.pop()
             s.add(z3.Or(db_var != s.model()[db_var], db_distance_var != s.model()[db_distance_var]))
         else:
-            print('not sat')
-            s.pop()
+            assert False, 'Cannot satisfy the constraints'
 
+    print(d1, d2)
     return d1, d2
 
