30acadbcb9585c08226ba81eee22d70553c5f185
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-01-30 16:10:52 -0500

    Put sig_test_stat and test_state into one package.

--- lightdp/stattest/test_stat.py
+++ lightdp/stattest/test_stat.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def test_stat(x, y, n, eps):
     """
         Function to compute test statistic T
@@ -8,3 +9,19 @@ def test_stat(x, y, n, eps):
     :return: T
     """
     return (len(x) / float(n)) - (np.exp(eps)) * (len(y) / float(n))  # one of the options
+
+
+def sig_test_stat(R, eps, n):
+    """
+        Function to compute significance of the value of T
+    :param R: x and y
+    :return: test statistic
+    """
+
+    Rcopy = R[:]
+    np.random.shuffle(Rcopy)
+    k = np.random.binomial(len(Rcopy), np.exp(eps) / (1 + (np.exp(eps))))
+    X = Rcopy[:k]
+    Y = Rcopy[k:]
+
+    return test_stat(X, Y, n, eps)
