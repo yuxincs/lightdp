0d2111c521a931ce68e56f73b07b917b23f68ce6
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-01-31 12:18:26 -0500

    Use S = [8, 4, 0].

--- lightdp/stattest/selectors.py
+++ lightdp/stattest/selectors.py
@@ -4,6 +4,6 @@ from inspect import isfunction
 def s_selector(algorithm):
     assert isfunction(algorithm)
     if algorithm.__name__ == 'noisymax':
-        return [7, 4, 2]
+        return [8, 4, 0]
     if algorithm.__name__ == 'sparsevector':
         return [9]
