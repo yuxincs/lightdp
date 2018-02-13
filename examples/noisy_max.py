29b9b6ba6a8aaf01927979273db7f5eaa40f3710
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-02-13 14:16:49 -0500

    Add notes for noisymax.

--- examples/noisy_max.py
+++ examples/noisy_max.py
@@ -1,8 +1,10 @@
-def exponential(epsilon, N, q):
+def noisymax(epsilon, N, q):
     """
     precondition : forall i (^q[i] >= -1 and ^q[i] <= 1);
+    q : list num(*); out : num(*); N : num(0);
     i, bq, cq : num(0); eta : num(-^q[i]); max_num : num(0)
     """
+    out = 0
     i = 0
     bq = 0
     cq = 0
@@ -10,6 +12,6 @@ def exponential(epsilon, N, q):
         eta = Laplace(2 / epsilon)
         cq = q[i] + eta
         if cq > bq and i == 1:
-            max_num = i
+            out = i
             bq = cq
         i = i + 1
