5902b3bf4b74802a9a47b4d992d63b752fabb3d0
GuanhongW <edgw564731@gmail.com>
2018-02-13 21:45:08 +0000

    standard deviation selector

--- lightdp/stattest/selectors.py
+++ lightdp/stattest/selectors.py
@@ -30,3 +30,28 @@ def difference_s_selector(algorithm, args, kwargs, D1, D2, iterations=1000):
     diff = sorted(diff, key=lambda k: k[1], reverse=True)
 
     return [diff[i][0] for i in range(1)]
+    
+    def sd_s_selector(algorithm, args, kwargs, D1, D2, iterations=1000):
+    assert isfunction(algorithm)
+    a = [algorithm(D1, *args, **kwargs) for _ in range(iterations)]
+    b = [algorithm(D2, *args, **kwargs) for _ in range(iterations)]
+    c=10
+
+    Scand=list(range(0,len(D1)))
+    max=0
+    maxi=0
+
+    for i in Scand:
+        p=(a.count(i)+c) / (len(a)+c)
+        q=(b.count(i)+c) / (len(b)+c)
+
+        if p==0 and q==0:
+            sddiff=0
+        else:
+            sddiff=abs((p-q)/((p*(1-p) + q*(1-q))**(1/2)))
+
+        if max<sddiff:
+            max=sddiff
+            maxi=i
+
+    return [maxi]
