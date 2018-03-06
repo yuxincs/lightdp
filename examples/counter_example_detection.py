eee97e3e0a3f420aa431cdd16c8d1b2c385e7ae3
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-03-06 15:11:39 -0500

    Update new framework example.

--- examples/counter_example_detection.py
+++ examples/counter_example_detection.py
@@ -1,6 +1,6 @@
-from lightdp.stattest import hypothesis_test, difference_test_stat, division_test_stat, \
-    sig_test_stat, generate_inputs, difference_s_selector
-from lightdp.stattest.algorithms import noisymax, sparsevector
+from lightdp.stattest import hypothesis_test, generate_inputs, fisher_s_selector, difference_s_selector
+from lightdp.stattest.algorithms import noisy_max_v1, sparse_vector
+from interval import interval, inf, imath
 
 
 def main():
@@ -15,26 +15,21 @@ def main():
     D2 = [5.21, 3.52, 5.57, 3.18, 5.6, 5.1, 4.15, 5.72, 5.99, 5.32]
 
     # auto-generated data for sparsevector
+    # D1 = [10.747997673023637, 9.176688735248929, 9.685779806342222, 9.276506422051003, 10.14197198493314, 10.777285751217708, 10.033518297158158, 10.739107887579431, 9.466024642462145, 10.046506781898248]
+    # D2 = [9.74799, 9.999989999999999, 10.19486, 9.99999, 9.57099, 9.888639999999999, 9.08919, 9.76694, 9.99999, 9.73776]
 
-    """
-    D1 = [9.917370926052758, 10.295232500311627, 10.979253058964114, 10.954906274017656,
-          9.205904120344211, 10.52166922332997, 9.17760719515679, 9.830086654150426, 9.544008376222147,
-          10.034087353065718]
-
-    D2 = [9.91737, 10.0, 10.0, 10.31766, 8.39766, 9.62791, 9.99999, 10.48255, 9.99999, 9.86943]
-    """
-    test_epsilon = 2
-    algorithm = noisymax
+    D1 = [10.047838788362302, 9.156301318712105, 9.934576874381165, 10.818923545533439, 10.964414221707846, 9.814549818258723, 10.343685529387757, 10.270895438249106, 10.422325599687007, 9.498322242077997]
+    D2 = [11.04783, 10.10206, 10.85322, 10.04784, 10.04784, 10.40406, 10.04784, 9.664679999999999, 9.660929999999999, 10.06784]
+    test_epsilon = 1
+    algorithm = noisy_max_v1
     results = []
     args = ()
-    for eps in [x / 10.0 for x in range(29, 50, 1)]:
+    for eps in [x / 10.0 for x in range(1, 30, 1)]:
         kwargs = {'eps': eps}
         avg_p1, avg_p2 = 0, 0
-        S = difference_s_selector(algorithm, args, kwargs, D1, D2)
+        S = fisher_s_selector(algorithm, args, kwargs, D1, D2, search_space=[[i] for i in range(10)])
         for i in range(5):
-            p1, p2 = hypothesis_test(algorithm, args, kwargs, D1, D2,
-                                     S,
-                                     difference_test_stat(test_epsilon), sig_test_stat(test_epsilon), 100000, 0)
+            p1, p2 = hypothesis_test(algorithm, args, kwargs, D1, D2, S, test_epsilon, 10000, cores=1)
             avg_p1 += p1
             avg_p2 += p2
         print("epsilon: %f, p1 = %f, p2 = %f | S = %s" % (eps, avg_p1 / 5, avg_p2 / 5, S))
