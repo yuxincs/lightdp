2cf7cfeab302459c547bb37b1d7f559ca1c30674
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-03-08 14:04:23 -0500

    Rewrite the fisher_s_selector for better efficiency.

--- lightdp/stattest/selectors.py
+++ lightdp/stattest/selectors.py
@@ -60,23 +60,20 @@ def sd_s_selector(algorithm, args, kwargs, D1, D2, iterations=1000):
     return [maxi]
 
 
-def fisher_s_selector(algorithm, args, kwargs, D1, D2, iterations=100000, search_space=[]):
+def fisher_s_selector(algorithm, args, kwargs, D1, D2, epsilon, iterations=1000, search_space=[]):
     assert isfunction(algorithm)
+    from .core import test_statistics
+
     a = [algorithm(D1, *args, **kwargs) for _ in range(iterations)]
     b = [algorithm(D2, *args, **kwargs) for _ in range(iterations)]
 
-    p = 1
-    s_chosen = search_space[0]
-
+    # find S which has minimum p value from search space
+    p, s_chosen = 1, search_space[0]
     for s in search_space:
-        a_s = [x for x in a if x in s]
-        b_s = [y for y in b if y in s]
-        c_x = max(len(a_s), len(b_s))
-        c_y = min(len(a_s), len(b_s))
-        c_xx = sum(stats.binom.rvs(n = c_x, p = 1/math.e**kwargs['eps'], size = iterations))/iterations
-        q = 1 - stats.hypergeom.cdf(c_xx, 2 * iterations, iterations, c_xx + c_y)
-        if p > q:
-            p = q
-            s_chosen = s
+        cx = sum(1 for x in a if x in s)
+        cy = sum(1 for y in b if y in s)
+        cx, cy = (cx, cy) if cx > cy else (cy, cx)
+        new_p = test_statistics(cx, cy, epsilon, iterations)
+        p, s_chosen = (new_p, s) if new_p < p else (p, s_chosen)
 
     return s_chosen
