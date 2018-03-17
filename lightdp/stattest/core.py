0265bb8d485d664c1102bdfa75d1f66e8ab1bddb
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-03-16 23:55:40 -0400

    Use multiprocessing.Pool to do all the work.

--- lightdp/stattest/core.py
+++ lightdp/stattest/core.py
@@ -5,9 +5,6 @@ import codecs
 import os
 from scipy import stats
 
-# global process pool
-_process_pool = mp.Pool(mp.cpu_count())
-
 
 class __HyperGeometric:
     """
@@ -15,11 +12,38 @@ class __HyperGeometric:
     which only accepts pickle-able functions or objects.
     """
     def __init__(self, cy, iterations):
-        self.__cy = cy
-        self.__iterations = iterations
+        self.cy = cy
+        self.iterations = iterations
 
     def __call__(self, cx):
-        return 1 - stats.hypergeom.cdf(cx, 2 * self.__iterations, self.__iterations, cx + self.__cy)
+        np.random.seed(int(codecs.encode(os.urandom(4), 'hex'), 16))
+        return 1 - stats.hypergeom.cdf(cx, 2 * self.iterations, self.iterations, cx + self.cy)
+
+
+class __RunAlgorithm:
+    """
+    Used by hypothesis_test to run algorithm using different database concurrently.
+    """
+    def __init__(self, algorithm, args, kwargs, D1, D2, S):
+        self.algorithm = algorithm
+        self.args = args
+        self.kwargs = kwargs
+        self.D1 = D1
+        self.D2 = D2
+        self.S = S
+
+    def __call__(self, iterations):
+        return self.run(iterations)
+
+    def run(self, iterations):
+        np.random.seed(int(codecs.encode(os.urandom(4), 'hex'), 16))
+        cx = sum(1 for _ in range(iterations) if self.algorithm(self.D1, *self.args, **self.kwargs) in self.S)
+        cy = sum(1 for _ in range(iterations) if self.algorithm(self.D2, *self.args, **self.kwargs) in self.S)
+        return cx, cy
+
+
+# global process pool
+_process_pool = mp.Pool(mp.cpu_count())
 
 
 def test_statistics(cx, cy, epsilon, iterations):
@@ -39,34 +63,6 @@ def test_statistics(cx, cy, epsilon, iterations):
                                      np.random.binomial(cx, 1.0 / (np.exp(epsilon)), 1000), int(1000 / mp.cpu_count())))
 
 
-def _run_algorithm(algorithm, args, kwargs, D1, D2, S, iterations):
-    cx = sum(1 for _ in range(iterations) if algorithm(D1, *args, **kwargs) in S)
-    cy = sum(1 for _ in range(iterations) if algorithm(D2, *args, **kwargs) in S)
-
-    return cx, cy
-
-
-def _multiprocessing_run(func, args, iterations, process_count):
-    def process_func(function, args, kwargs, result_queue):
-        np.random.seed(int(codecs.encode(os.urandom(4), 'hex'), 16))
-        result_queue.put(function(*args, **kwargs))
-
-    result_queue = mp.Queue()
-
-    processes = []
-    for p_id in range(process_count):
-        process_iterations = int(math.floor(float(iterations) / process_count))
-        process_iterations += iterations % process_iterations if p_id == process_count - 1 else 0
-        process = mp.Process(target=process_func, args=(func, args, {'iterations': process_iterations}, result_queue))
-        processes.append(process)
-        process.start()
-
-    for process in processes:
-        process.join()
-
-    return (result_queue.get() for _ in range(process_count))
-
-
 def hypothesis_test(algorithm, args, kwargs, D1, D2, S, epsilon, iterations, cores=1):
     """
     :param algorithm: The algorithm to run on
@@ -82,13 +78,18 @@ def hypothesis_test(algorithm, args, kwargs, D1, D2, S, epsilon, iterations, cor
     """
     np.random.seed(int(codecs.encode(os.urandom(4), 'hex'), 16))
     if cores == 1:
-        cx, cy = _run_algorithm(algorithm, args, kwargs, D1, D2, S, iterations)
+        cx, cy = __RunAlgorithm(algorithm, args, kwargs, D1, D2, S).run(iterations)
         cx, cy = (cx, cy) if cx > cy else (cy, cx)
         return test_statistics(cx, cy, epsilon, iterations), test_statistics(cy, cx, epsilon, iterations)
     else:
+        global _process_pool
         process_count = mp.cpu_count() if cores == 0 else cores
 
-        result = _multiprocessing_run(_run_algorithm, (algorithm, args, kwargs, D1, D2, S), iterations, process_count)
+        process_iterations = [int(math.floor(float(iterations) / process_count)) for _ in range(process_count)]
+        # add the remaining iterations to the last index
+        process_iterations[process_count - 1] += iterations % process_iterations[process_count - 1]
+
+        result = _process_pool.map(__RunAlgorithm(algorithm, args, kwargs, D1, D2, S), process_iterations)
 
         cx, cy = 0, 0
         for process_cx, process_cy in result:
