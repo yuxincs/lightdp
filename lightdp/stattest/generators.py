66974767aecb153482beb5ced64b8bdd30222df9
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-03-24 12:16:55 -0400

    Implement simple input generator.

--- lightdp/stattest/generators.py
+++ lightdp/stattest/generators.py
@@ -161,7 +161,8 @@ def symbolic_generator(tree, args, num_input, fixed_d1=[]):
     return d1, d2
 
 
-def simple_generator(algorithm, args, kwargs, num_input):
+# TODO: to remove manual search space in the future
+def simple_generator(algorithm, args, kwargs, num_input, search_space):
     """
     :param algorithm: The algorithm to run.
     :param args: The argument for the algorithm.
@@ -169,8 +170,55 @@ def simple_generator(algorithm, args, kwargs, num_input):
     :param num_input: The number of inputs to be generated
     :return: Database (d1, d2)
     """
+    assert isinstance(args, tuple)
+    assert isinstance(kwargs, dict)
     from .core import hypothesis_test
     from .selectors import fisher_s_selector
-    d1, d2 = [], []
+    import numpy as np
+    import math
+    # assume maximum distance is 1
+    d1 = [1 for _ in range(num_input)]
+    candidates = [
+        (d1, [0] + [1 for _ in range(num_input - 1)]),  # one below
+        (d1, [2] + [1 for _ in range(num_input - 1)]),  # one above
+        (d1, [2] + [0 for _ in range(num_input - 1)]),  # one above rest below
+        (d1, [0] + [2 for _ in range(num_input - 1)]),  # one below rest above
+        (d1, [2 for _ in range(int(num_input / 2))] + [0 for _ in range(num_input - int(num_input / 2))]),  # half half
+        (d1, [2 for _ in range(num_input)]),  # all above
+        (d1, [0 for _ in range(num_input)]),  # all below
+    ]
 
-    return d1, d2
+    # TODO: maybe we should test many algorithm epsilon and average
+    algorithm_epsilon = 1
+    results = []
+
+    for d1, d2 in candidates:
+        kwargs['eps'] = algorithm_epsilon
+
+        rising_epsilon = -math.inf
+        steady_epsilon = math.inf
+        previous_p = [0.0, 0.0]
+        for test_epsilon in np.arange(max(algorithm_epsilon - 0.5, 0.1), algorithm_epsilon + 2.0, 0.1):
+            s = fisher_s_selector(algorithm, args, kwargs, d1, d2, test_epsilon, search_space=search_space)
+            p1, p2 = hypothesis_test(algorithm, args, kwargs, d1, d2, s, test_epsilon, iterations=100000, cores=0)
+
+            rising_epsilon = test_epsilon if p1 < 0.05 and previous_p < [0.05, 0.05] else rising_epsilon
+
+            steady_epsilon = test_epsilon if p1 > 0.95 and previous_p > [0.95, 0.95] else steady_epsilon
+
+            # store the new p into the history
+            previous_p[0], previous_p[1] = previous_p[1], previous_p[0]
+            previous_p[0] = p1
+
+            # stop early for best performance
+            if rising_epsilon > -math.inf and steady_epsilon < math.inf:
+                break
+
+        # prevent inserting 'nan'
+        if rising_epsilon == -math.inf or steady_epsilon == math.inf:
+            results.append(math.inf)
+        else:
+            results.append((steady_epsilon - rising_epsilon) * 1.0 / rising_epsilon)
+
+
+    return candidates[np.argmin(results)]
