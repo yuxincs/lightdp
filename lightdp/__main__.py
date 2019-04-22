8fb7cc3ed54b6240978048480c49ecfb04a1a835
Yuxin Wang <yuxinwang.dev@gmail.com>
2019-04-22 13:36:43 -0400

    Output the constraints and transformed code in the current directory.

--- lightdp/__main__.py
+++ lightdp/__main__.py
@@ -1,6 +1,14 @@
 import ast
+import argparse
+import astunparse
+import logging
+import os
+import coloredlogs
 from lightdp import __doc__
 from lightdp import verify, transform
+
+coloredlogs.install('INFO', fmt='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -8,43 +16,39 @@ def main():
     Main function, parse the program argument and run the program.
     :return: None
     """
-    import argparse
-    import astunparse
 
     arg_parser = argparse.ArgumentParser(description=__doc__)
     arg_parser.add_argument('file', metavar='FILE', type=str, nargs=1)
     arg_parser.add_argument('-o', '--out',
                             action='store', dest='out', type=str,
                             help='The output file name.', required=False)
-    arg_parser.add_argument('-j', '--json',
-                            action='store', dest='json_file', type=str,
-                            help='Output all results in json format.', required=False)
+    arg_parser.add_argument('-c', '--constraints',
+                            action='store', dest='constraints', type=str,
+                            help='Output all constraints to file.', required=False)
 
     results = arg_parser.parse_args()
     results.file = results.file[0]
-    results.out = results.file[0:results.file.rfind('.')] + '_t.py' if results.out is None else results.out
+    results.out = results.out if results.out else \
+        './{}_t.py'.format(os.path.splitext(os.path.basename(results.file))[0])
+    results.constraints = results.constraints if results.constraints else \
+        './{}_constraints.txt'.format(os.path.splitext(os.path.basename(results.file))[0])
 
     # parse / verify / transform the file
     with open(results.file, 'r') as f:
         tree = ast.parse(f.read())
         is_sat, constraints = verify(tree)
-        transformed = None
         if not is_sat:
+            logger.info('Type check succeeds, now transforming the algorithm...')
             with open(results.out, 'w') as out:
-                transformed = astunparse.unparse(transform(tree))
-                out.write(transformed)
+                out.write(astunparse.unparse(transform(tree)))
+            logger.info('Transformation finished at {}. See {} for constraints generated.'
+                        .format(results.out, results.constraints))
+        else:
+            logger.info('Type check fails, the algorithm does not satisfy differential privacy.'
+                        'See {} for constraints used.'.format(results.constraints))
 
-        if results.json_file is not None:
-            with open(results.json_file, 'w') as out:
-                import json
-                json.dump({
-                    'is_sat': is_sat,
-                    'constraints': constraints,
-                    'transformed': transformed
-                }, out)
-
-        # print('\033[32;1mFinal Constraint:\033[0m')
-        # print(in_file + ' : satisfiable' if is_sat else ' : unsatisfiable')
+        with open(results.constraints, 'w') as out:
+            out.write(constraints)
 
 
 if __name__ == '__main__':
